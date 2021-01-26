df94f6a778aa1147156b259474baf66339bc2290
Juan Cruz <jc23729@gmail.com>
2021-01-26 20:52:31 +0000

    class LoginForm

---
+++ myproject/forms.py
@@ -0,0 +1,12 @@
+#forms.py
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired,Email,EqualTo
+from wtforms import ValidationError
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
+    
