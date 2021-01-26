616443939c36315697c84288de38659b1580785a
Juan Cruz <jc23729@gmail.com>
2021-01-26 21:03:38 +0000

    setup imports in app.py

--- myproject/app.py
+++ myproject/app.py
@@ -1,5 +1,9 @@
-#Password Hashing
-# pip install flask-bcrypt(on command line)
-from flask import Flask
-from flask_bcrypt import Bcrypt
+#app.py
+
+from myproject import app,db
+from flask import render_template, redirect, request, url_for, flash,abort
+from flask_login import login_user,login_required,logout_user
+from myproject.models import User
+from myproject.forms import LoginForm, RegistrationForm
+from werkzeug.security import generate_password_hash, check_password_hash
 
