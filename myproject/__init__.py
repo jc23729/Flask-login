88a132bbdebd5657eb0482d156c00a1ef810e131
Juan Cruz <jc23729@gmail.com>
2021-01-26 20:08:11 +0000

    pass in our app to login manager

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -17,3 +17,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+Migrate(app,db)
+
+# We can now pass in our app to the login manager
+login_manager.init_app(app)
