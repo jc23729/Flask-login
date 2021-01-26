d99b5e67836caf9aa9d749d738d1a5583d5652dc
Juan Cruz <jc23729@gmail.com>
2021-01-26 20:05:36 +0000

    update

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -6,3 +6,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+login_manager = LoginManager()
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'mysecretkey'
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
